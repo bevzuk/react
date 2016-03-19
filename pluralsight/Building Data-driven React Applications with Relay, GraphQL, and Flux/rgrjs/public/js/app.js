cb1ccabcc6cae7b9b88ec92ae7c71e8804c1162e
Anton Bevzuk <anton.bevzuk@smartstepgroup.com>
2016-03-19 15:43:28 +0300

    webpack

--- pluralsight/Building Data-driven React Applications with Relay, GraphQL, and Flux/rgrjs/public/js/app.js
+++ pluralsight/Building Data-driven React Applications with Relay, GraphQL, and Flux/rgrjs/public/js/app.js
@@ -1,1 +1,8 @@
-console.log("Hello JS");
+import React from "react";
+import ReactDOM from "react-dom";
+import Hello from "./components/Hello.js";
+
+ReactDOM.render(
+    <Hello />,
+    document.getElementById("react")
+);
